2d11427000b713e938e2183bd38778afc3308d2f
Nairb <ngvarona@student.apc.edu.ph>
2026-01-19 21:31:11 +0800

    added design

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,22 +1,66 @@
 <template>
 <div class="container">
-    <header> <h1> Personal Profile Web page </h1>  </header>
-    <main>
-      <section class="main-content">
-        <div>1. About me</div>
-        <div>2. Education/Achievements</div>
-        <div>3. Course</div>
-        <div>4. IT Experience</div>
-        <div>5. Hobbies and Interests</div>
-        <div>6. Goals in Life</div>
-        <div>7. Picture Gallery</div>
-      </section>
-      <aside class="sidebar">
-        </aside>
-    </main>
-    <footer> Asia Pacific College </footer>
+    <header><h1>Nairb's Profile Web page </h1>  </header>
+  <div>About Me<br>
+    Musika ang buhay na aking tinataglay, ito rin ang dahilan kung bat ako naglalakbay<br></div>
+  <div>Education/Achievements: <br>
+    2nd year college<br></div>
+  <div>Course: <br>
+    Bachelor of Science in Information Technology <br></div>  
+  <div>IT Experience:<br>
+    Gaming<br>
+    Cybersecurity Seminar<br>
+    LinkedIn Certifications<br></div>
+  <div>Hobbies & Interests:<br>
+    Playing band instruments<br>
+    Playing online games<br></div>
+  <div>Goals In Life / Dream:<br>
+    Have time and financial freedom<br>
+    Be able to produce my own music<br></div> 
  </div>
 </template>
 
 <script></script>
-<style></style>
+<style>
+    .container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(170, 225, 243);
+}
+
+.container div {
+  background-color: #c5f3ff;
+  width: 300px;
+  margin: 20px;
+  padding: 10px;
+  border-radius: 16px;
+  text-align: center;  
+  text-shadow: 4px 3px 4px #71a8cc;
+  font-family: Verdana, Geneva, sans-serif;
+  font-size: 16px;
+  letter-spacing: -1px;
+  word-spacing: 2px;
+  color: #000000;
+  font-weight: 400;
+  text-decoration: none;
+  font-style: normal;
+  font-variant: normal;
+  text-transform: none;
+}
+
+body {
+  background-color: rgba(209, 255, 252, 0.714);
+}
+
+h1 {
+  color: rgb(0, 0, 46);
+  text-align: center;
+}
+
+p {
+  text-align: center;
+  color: rgb(0, 0, 0);
+}
+</style>
