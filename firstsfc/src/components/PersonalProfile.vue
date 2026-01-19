d1cfb9fd22254f22de9befeb72e60e6dd5960ddd
Nairb <ngvarona@student.apc.edu.ph>
2026-01-19 17:53:23 +0800

    tried fixing it again

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,29 +1,21 @@
 <template>
 <div class="container">
-    <header> <h1>My Profile Web page </h1>  </header>
+    <header> <h1> Personal Profile Web page </h1>  </header>
     <main>
       <section class="main-content">
-            <div>About Me<br>
-            Musika ang buhay na aking tinataglay, ito rin ang dahilan kung ba't ako naglalakbay.<br></div>
-            <div>Education/Achievements: <br>
-            2nd year college<br></div>
-            <div>Course: <br>
-            Bachelor of Science in Information Technology <br></div>  
-            <div>IT Experience:<br>
-            Gaming<br>
-            Cybersecurity Seminar<br>
-            LinkedIn Certifications<br></div>
-            <div>Hobbies & Interests:<br>
-            Playing band instruments<br></div>
-            <div>Goals In Life / Dream:<br>
-            Have time and financial freedom<br>
-            Be able to produce my own music<br></div>
-        </section>
-        <aside class="sidebar">
-            </aside>
-          </main>
-          <footer> Asia Pacific College </footer>
-        </div>
+        <div>1. About me</div>
+        <div>2. Education/Achievements</div>
+        <div>3. Course</div>
+        <div>4. IT Experience</div>
+        <div>5. Hobbies and Interests</div>
+        <div>6. Goals in Life</div>
+        <div>7. Picture Gallery</div>
+      </section>
+      <aside class="sidebar">
+        </aside>
+    </main>
+    <footer> Asia Pacific College </footer>
+ </div>
 </template>
 
 <script></script>
